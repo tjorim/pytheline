38ba70bc030774f50177c14ff3f2df0520ca2b7e
Jorim Tielemans <tielemans.jorim@gmail.com>
2018-08-13 19:03:42 +0200

    Extra functions
    and documentation

--- pytheline/pytheline.py
+++ pytheline/pytheline.py
@@ -6,12 +6,13 @@ base_url = 'https://www.delijn.be/{}/'
 
 rise_api = {
     'core': 'rise-api-core'
-    }
+}
 
 
 class deLijn:
 
     def __init__(self):
+        pass
 
     def do_request(self, api, params=None):
         if api in rise_api:
@@ -38,27 +39,117 @@ class deLijn:
                     return -1
 
     def convert_location(self, lat=None, long=None):
+        """
+        The rise-API uses x/y coordinates for all geo-based information, Belgian Lambert 72 coordinates.
+        This function converts proper latitude/longitude coordinates into their equivalent x/y coordinates.
+
+        ...
+
+        Parameters
+        ----------
+        lat : float
+            latitude coordinate
+        long : float
+            longitude coordinate
+
+        Returns
+        -------
+        json which contains the following values
+            xCoordinaat : int
+                X coordinate
+            yCoordinaat : int
+                Y coordinate
+
+        {
+          "halteNummer": null,
+          "idString": null,
+          "laatstgebruikt": null,
+          "locationId": null,
+          "markedString": null,
+          "xCoordinaat": 103853,
+          "yCoordinaat": 191981
+        }
+
+        """
         if lat and long:
             params = ['locations', 'convert', lat, long]
             json_data = self.do_request('core', params)
             return json_data
 
-# They seem to use x/y coordinates for all their geo-based information
-# so they have an API route that converts proper lat/long coordinates into their equivalent x/y coordinates.
-# Use the following route to convert lat/long to x/y where {{lat}} is your latitude and {{long}} is your longitude.
+    def haltes_doorkomendelijnen(self, halte_id=None):
+        """
 
-# Which will give you the x/y coordinate in a response similar to:
+        ...
 
-# {
-  # "halteNummer": null,
-  # "idString": null,
-  # "laatstgebruikt": null,
-  # "locationId": null,
-  # "markedString": null,
-  # "xCoordinaat": 103853,
-  # "yCoordinaat": 191981
-# }
+        Parameters
+        ----------
+        halte_id : int
+            Halte ID
 
-# In my opinion, they should just stick to using the familiar lat/long coordinates on the frontend and silently convert them to their x/y equivalents on the backend. That way the client doesn't have to do an additional API request for the conversion and we can all work with data that we are most familiar with.
+        Returns
+        -------
+        """
+        if halte_id:
+            params = ['haltes', 'doorkomendelijnen', halte_id]
+            json_data = self.do_request('core', params)
+            return json_data
 
-# EDIT: A reader has notified me that these are most likely Belgian Lambert 72 coordinates.
+    def haltes_indebuurt(self, xCoordinaat=None, yCoordinaat=None, radius=None):
+        """
+
+        ...
+
+        Parameters
+        ----------
+        xCoordinaat : int
+            X coordinate
+        yCoordinaat : int
+            Y coordinate
+        radius : int
+            Zoek radius, eenheid onbekend
+
+        Returns
+        -------
+        """
+        if halte_id:
+            params = ['haltes', 'indebuurt', xCoordinaat, yCoordinaat, radius]
+            json_data = self.do_request('core', params)
+            return json_data
+
+    def haltes_titel(self, halte_id=None):
+        """
+
+        ...
+
+        Parameters
+        ----------
+        halte_id : int
+            Halte ID
+
+        Returns
+        -------
+        """
+        if halte_id:
+            params = ['haltes', 'titel', halte_id]
+            json_data = self.do_request('core', params)
+            return json_data
+
+    def haltes_vertrekken(self, halte_id=None, num_results=8):
+        """
+
+        ...
+
+        Parameters
+        ----------
+        halte_id : int
+            Halte ID
+        num_results : int, optional
+            Maximum aantal resultaten
+
+        Returns
+        -------
+        """
+        if halte_id:
+            params = ['haltes', 'vertrekken', halte_id, num_results]
+            json_data = self.do_request('core', params)
+            return json_data
--- tests/test_delijn.py
+++ tests/test_delijn.py
@@ -4,5 +4,17 @@ from pytheline import deLijn
 def test_delijn_halte():
 
     delijn_instance = deLijn()
-    response = delijn_instance.convert_location(51.2, 17, 2)
+    response = delijn_instance.convert_location(51.1, 5.1)
+    print(response)
+
+    response = delijn_instance.haltes_doorkomendelijnen(51.1, 5.1)
+    print(response)
+
+    response = delijn_instance.haltes_indebuurt(51.1, 5.1, 300)
+    print(response)
+
+    response = delijn_instance.haltes_titel(51.1, 5.1)
+    print(response)
+
+    response = delijn_instance.haltes_vertrekken(401474, 8)
     print(response)
